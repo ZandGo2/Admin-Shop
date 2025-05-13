f8a9597d2a99fbf7b4c2aa3085a43df2231c1e0c
“[Mobin <[zandgo2@gmail.com]>
2025-05-13 19:34:39 +0330

    make logic for delete component

--- src/components/DeleteProductModal.jsx
+++ src/components/DeleteProductModal.jsx
@@ -1,19 +1,46 @@
 import deleteIcon from "../asset/images/Close.png";
 import styles from "./modal.module.css";
+import { DeleteProductApi } from "../services/api";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { notify } from "../utils/notify";
 
 const DeleteProductModal = ({ close, id, setDeleteItem }) => {
+  const queryClient = useQueryClient();
+
+  const mutationFn = async (id) => {
+    DeleteProductApi(id);
+  };
+
+  const { mutate } = useMutation({ mutationFn });
+
+  const clickHandler = () => {
+    mutate(id, {
+      onSuccess: (data) => {
+        notify("success", "Deleted successfully");
+        queryClient.invalidateQueries({ queryKey: ["ListProduct"] });
+        close(setDeleteItem);
+      },
+      onError: (error) => {
+        notify("error", error.response?.data.message);
+        console.log(error);
+      },
+    });
+  };
+
   return (
     <div className={styles.blurDiv}>
       <div className={styles.containerDelete}>
         <img src={deleteIcon} alt="deleteIcon" />
         <div className={styles.containerDeleteDivBtn}>
           <p>Are you sure about deleting the product ?</p>
-          <button className={styles.btnDeleteCancel}>Delete</button>
+          <button className={styles.btnDeleteCancel} onClick={clickHandler}>
+            Delete
+          </button>
           <button
             className={styles.btnDelete}
             onClick={() => close(setDeleteItem)}
           >
-            Cancellation{" "}
+            Cancellation
           </button>
         </div>
       </div>
--- src/services/api.js
+++ src/services/api.js
@@ -12,4 +12,8 @@ const GetProductsApi = (page, name) => {
   return axiosInstance.get(`/products?page=${page}&limit=8&name=${name}`);
 };
 
-export { signUPApi, loginApi, GetProductsApi };
+const DeleteProductApi = (id) => {
+  return axiosInstance.delete(`/products/${id}`);
+};
+
+export { signUPApi, loginApi, GetProductsApi, DeleteProductApi };
